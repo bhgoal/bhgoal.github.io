e5c9a6f6122dfe56c3f89bec647ebd26f2d3f393
gorbasho <37026147+gorbasho@users.noreply.github.com>
2018-05-03 19:40:50 -0500

    works section updated

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,30 +1,30 @@
 $("body").scrollspy({target: "#navbarSupportedContent"})
 
 
-    // Add smooth scrolling to all links
-    $(".nav-item").on("click", function(event) {
-		$("body").scrollspy("dispose")
-		$("a").removeClass("active");
-		$(this).addClass("active");
-		// Make sure this.hash has a value before overriding default behavior
-		if (this.hash !== "") {
-			// Prevent default anchor click behavior
-			event.preventDefault();
-	
-			// Store hash
-			var hash = this.hash;
-	
-			// Using jQuery's animate() method to add smooth page scroll
-			// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-			$('html, body').animate({
-				scrollTop: $(hash).offset().top
-			}, 800, function(){
-				// Add hash (#) to URL when done scrolling (default click behavior)
-				window.location.hash = hash;
-				$("body").scrollspy({target: "#navbarSupportedContent"})
-			});
-		}
-    });
+// Add smooth scrolling to all links
+$(".nav-item").on("click", function(event) {
+	$("body").scrollspy("dispose")
+	$("a").removeClass("active");
+	$(this).addClass("active");
+	// Make sure this.hash has a value before overriding default behavior
+	if (this.hash !== "") {
+		// Prevent default anchor click behavior
+		event.preventDefault();
+
+		// Store hash
+		var hash = this.hash;
+
+		// Using jQuery's animate() method to add smooth page scroll
+		// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+		$('html, body').animate({
+			scrollTop: $(hash).offset().top
+		}, 800, function(){
+			// Add hash (#) to URL when done scrolling (default click behavior)
+			window.location.hash = hash;
+			$("body").scrollspy({target: "#navbarSupportedContent"})
+		});
+	}
+});
 
 
 
@@ -32,23 +32,25 @@ $('#list-tab a').on('click', function (e) {
 	e.preventDefault();	
 	e.stopPropagation();
 	$(this).tab('show')
+	setTimeout(captionReset, 200);
+	setTimeout(captionFadeIn, 1000);
 })
 
+setTimeout(captionFadeIn, 1000);
 
 
 
 
 
+function captionReset() {
+	$(".imgCaption").css("top", "500px");
+};
 
 
-
-
-
-
-
-
-
-
+function captionFadeIn() { 
+ //$(".imgCaption").fadeIn(1000); //.fadeIn(250)
+ $(".imgCaption").animate({top: "100px"});
+};
 
 
 
